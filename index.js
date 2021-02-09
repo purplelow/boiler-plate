f7feb1a69ddaa97a61057eb0bc559fb034644c85
Js-MacBook Pro <purplelow1@gmail.com>
2021-02-09 10:15:12 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://jscreation:abcd1234@youtuclone.cz49t.mongodb.net/YoutuClone?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
+app.get('/', (req, res) => {
+  res.send('Hello World! Hi~~')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
